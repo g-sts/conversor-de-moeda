6e745bc9302dfa8f89f7860e9772855da0e6bc04
git status <215340863+g-sts@users.noreply.github.com>
2025-06-18 20:23:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-let form = document.getElementById("converterForm")
+const form = document.getElementById("converterForm")
+const amaount = document.getElementById("amount")
+const fromCurrency = document.getElementById("fromCurrency")
+const convertedAmount = document.getElementById("convertedAmount")
+const toCurrency = document.getElementById("toCurrency")
+const loading = document.querySelector(".loading")
+const result = document.querySelector(".result")
+const error = document.querySelector(".error")
 
-function convertMoney(){
-    console.log("Funcionou")
+const API_URL = "https://api.exchangerate-api.com/v4/latest/"
+
+async function convertMoney(){
+
+    loading.style.display = "block"
+    
+    try{
+        const response = await fetch(API_URL + fromCurrency.value)
+        const data = await response.json()
+
+        const rate = data.rates[toCurrency.value]
+        const convertedRate = (amaount.value * rate).toFixed(2)
+
+        console.log(convertedRate)
+    }
+
+    catch(error){
+        alert("Falha no servidor")
+    }
 
 }
 
